dd3b8e386aa6e3a27327849f7862d524122470fe
ACA-FY21 <70221245+ACA-FY21@users.noreply.github.com>
2022-02-26 21:42:32 -0700

    Add files via upload

---
+++ coding-exercises-examples/javascript-exercises-answers/01_myForEach.js
@@ -0,0 +1,21 @@
+/**
+  Implement your own version or Array.forEach()
+  The function should take as input:
+    an array: arr 
+    a function: f
+  The function call f on each element sequentially
+  Example:
+    myForEach([1, 2, 3], v => console.log(v))
+    ->
+      1
+      2
+      3
+ */
+
+const myForEach = (arr, f) => {
+  for (let i = 0; i < arr.length; i++) {
+    f(arr[i])
+  }
+}
+
+module.exports = myForEach
