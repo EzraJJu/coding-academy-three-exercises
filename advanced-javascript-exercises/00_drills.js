434f56b9a1c300f7bd918d8aa4f5f5272d7392b4
Ezra Ju <96998749+EzraJJu@users.noreply.github.com>
2022-07-25 16:24:06 -0400

    problemset1

--- advanced-javascript-exercises/00_drills.js
+++ advanced-javascript-exercises/00_drills.js
@@ -10,26 +10,71 @@
 // SYNTAX NOTE: on a function with one parameter (input) the parenthesis are omitted
 
 // TODO - write a function which reverses the string
-const reverse = (str) => {}
+const reverse = (str) => {
+  let array = str.split('')
+  let revArray = array.reverse() 
+  return revArray.join('') 
+}
 
 // TODO - write a function which returns the factorial of a positive integer
-const factorial = (num) => {}
-
+const factorial = (num) => {
+let x = 1; 
+let i = num
+do{ 
+  x = x-1; 
+  i--;
+}
+while(i>0)
+}
 // TODO - write a function which combines two arrays into an array of 2-element arrays and returns -1
 // if the two arrays are of unequal length
 // Example: zip([1, 2, 3], [5, 3, 1]) === [[1, 5], [2, 3], [3, 1]]
-const zip = (arr1, arr2) => {}
+const zip = (arr1, arr2) => {
+  arr1.length === arr2.length ? arr1.map((el i) => [el, arr2[i]]) : -1
+}
 
 // TODO - Write a function which does the opposite of `zip()`
-const unzip = (arr) => {}
+const unzip = (arr) => {
+  let r1 = []
+  let r2 = [] 
+
+  arr.forEach(([x,y])=> {
+    r1.push(x)
+    r2.push(y)
+  })
+  return [r1, r2]
+}
 
 // TODO - write a function which shifts a string `num` characters to the right
 // Example = shiftRight("Hello", 3) === "lloHe"
-const shiftRight = (str, num) => {}
+const shiftRight = (str, num) => {
+  str.slice(str.length - num) + str.slice(0, str.legnth - num)
+}
 
 // CHALLENGE - write a function which returns the current date in the following string format:
 // "Today's date is January 7th, 2016. It is 11:37 in the morning."
-const announceDate = () => {}
+const announceDate = () => {
+  const date = new Date()
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ]
+  const hours = date.getHours()
+  return `Today's date is ${
+    months[date.getMonth()]  } ${date.getDay()}, ${date.getFullYear()}. It is ${hours}:${date.getMinutes()} in the ${
+      hours > 11 ? 'afternoon' : 'morning'
+    }.`
+}
 
 module.exports = {
   reverse,
